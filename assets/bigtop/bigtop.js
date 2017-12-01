8283aa2209511e01eaaeaecd5182db162ce94254
Ken Eucker <keneucker@gmail.com>
2017-12-01 14:03:11 -0800

    Window.open was accidentally being called twice.

--- assets/bigtop/bigtop.js
+++ assets/bigtop/bigtop.js
@@ -1,6 +1,6 @@
 var campaignClickUrl = '<%= clickUrl %>';
 var windowTarget = '<%= windowTarget %>';
 document.querySelector('.image').onclick = function() {
-	var win = window.open(window.open(campaignClickUrl, windowTarget));
+	var win = window.open(campaignClickUrl, windowTarget);
 	if(win) { win.focus(); }
 };
