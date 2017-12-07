3bc2485f58c9d1e55d66a1fa9d702e60208a4f35
Ken Eucker <keneucker@gmail.com>
2017-12-06 18:14:28 -0800

    Got some of the interactivity working. Next step is to add a node app that runs express so that this webform can interact with the underlying quasar installation to complete builds.

--- assets/quasarWebform/src/quasarWebform.jsx
+++ assets/quasarWebform/src/quasarWebform.jsx
@@ -3,21 +3,85 @@ import { render } from "react-dom";
 
 import Form from "react-jsonschema-form";
 
+let forms = [];
+
 const log = (type) => console.log.bind(console, type);
 
-window.quasarForms.forEach((form) => {
-    var div = document.createElement('div');
-    var formTitle = form.name;
-    div.id = formTitle;
+const postData = (uri, data, success = null) => {
+    return makeAsynCall(uri, success ? success : (res) => { console.log(`post to ${uri} successful`, res) }, 'POST', data);
+}
 
-    render((
-        <Form 
-            schema={form.schema}
-            uiSchema={form.uiSchema}
-            onChange={log("changed")}
-            onSubmit={log("submitted")}
-            onError={log("errors")} />
-    ), div);
+const makeAsynCall = (uri, cb, method = 'GET', content = {}) => {
+    return 
+        fetch(uri, {
+            method: method,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(content) })
+        .then((response) => {
+            console.log('hello');
+            // TODO: More intelligence here around the response
+            cb(response);
+        });
+}
 
-    document.getElementById("app").appendChild(div);
-});
+const onFormSubmitted = (e) => {
+    postData(`${window.location}:3720`, e.formData);
+}
+
+const createForms = () => {
+    window.quasarForms.forEach((form) => {
+        var div = document.createElement('div');
+        var formTitle = form.name;
+        div.id = formTitle;
+        div.style.display = 'none';
+        forms.push(formTitle);
+
+        render((
+            <Form 
+                schema={form.schema}
+                uiSchema={form.uiSchema}
+                onChange={log("changed")}
+                onSubmit={onFormSubmitted}
+                onError={log("errors")} />
+        ), div);
+
+        document.getElementById("app").appendChild(div);
+    });
+}
+
+const createDropdown = () => {
+    var outer = document.createElement('div');
+    var dropdown = document.createElement('select');
+    const selectOption = document.createElement("option");
+
+    selectOption.text = "Quasar To Build";
+    selectOption.value = false;
+    selectOption.setAttribute("disabled", 1);
+    selectOption.setAttribute("selected", 1);
+    dropdown.add(selectOption);
+
+    forms.forEach((form) => {
+        let option = document.createElement("option");
+        option.text = option.value = form;
+        dropdown.add(option);
+    });
+    dropdown.addEventListener("change", (t) => {
+        forms.forEach((form) => {
+            const formEl = document.getElementById(form);
+            if(form == t.target.value) {
+                formEl.style.display = "block";
+            } else {
+                formEl.style.display = "none";
+            }
+        });
+    });
+
+    outer.appendChild(dropdown);
+    document.body.prepend(outer);
+}
+
+createForms();
+createDropdown();
