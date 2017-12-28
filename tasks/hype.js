9a3293c6c4565bf988d92e91825bbe193f2cf78f
Ken Eucker <keneucker@gmail.com>
2017-12-28 10:22:25 -0800

    Reverted the hype back to it's original task queue before a change yesterday.

--- tasks/hype.js
+++ tasks/hype.js
@@ -11,7 +11,7 @@ const lib = fs.existsSync(`${config.assetsFolder}/${shim}`) ? require(`${config.
 let dtAdsArgs = {};
 
 const task = () => {
-	return lib.copyFilesFromSourcesFolderToOutput(dtAdsArgs)
+	return lib.unpackFiles(dtAdsArgs)
 		.then(() => { return lib.moveTargetFilesToRootOfAssetsPath(dtAdsArgs) })
 		.then(() => { return parseFiles() })
 		.then(() => { return confirmationPrompt() })
