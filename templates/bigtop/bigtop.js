78665e2b43568c7bf0b08211859122b0d426e24f
Ken Eucker <keneucker@gmail.com>
2017-12-29 20:28:33 -0800

    Removed coded uploaded accidentally.

--- templates/bigtop/bigtop.js
+++
@@ -1,6 +0,0 @@
-var campaignClickUrl = '<%= clickUrl %>';
-var clickTarget = '<%= clickTarget %>';
-document.querySelector('.image').onclick = function() {
-	var win = window.parent.open(campaignClickUrl, clickTarget);
-	if(win) { win.focus(); }
-};
